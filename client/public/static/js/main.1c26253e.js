3a9b9301479f0e5c67641f085efec087b3ccc722
Vrnx007 <runrainilluva@gmail.com>
2025-03-21 15:57:39 +0530

    main.1c26253e.js

--- client/public/static/js/main.1c26253e.js
+++ client/public/static/js/main.1c26253e.js
@@ -14,7 +14,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com/">
 <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap">
-<title>Sonivo - Ai call center</title>
+<title>PhoneticAI</title>
 <script defer src="./main.1c26253e.js"></script><link href="./../css/main.1c9f4024.css" rel="stylesheet">
 <script defer src="./main.29cdb0fd.js"></script><link href="./../css/main.67fdb4aa.css" rel="stylesheet">
 </head>
